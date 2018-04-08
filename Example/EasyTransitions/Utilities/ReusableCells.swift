09ab92cb8b7e460d6f8e4b675bdd60a89596c53a
Marcos Griselli <marcosgriselli@gmail.com>
2018-04-08 13:29:15 -0300

    Removed warnings

--- Example/EasyTransitions/Utilities/ReusableCells.swift
+++ Example/EasyTransitions/Utilities/ReusableCells.swift
@@ -33,18 +33,18 @@ extension UICollectionViewCell: ReusableView {}
 
 extension UITableView {
     
-    func register<T: UITableViewCell>(_: T.Type) where T: ReusableView {
+    func register<T: UITableViewCell>(_: T.Type) {
         register(T.self, forCellReuseIdentifier: T.defaultReuseIdentifier)
     }
     
-    func register<T: UITableViewCell>(_: T.Type) where T: ReusableView, T: NibLoadableView {
+    func register<T: UITableViewCell>(_: T.Type) where T: NibLoadableView {
         let bundle = Bundle(for: T.self)
         let nib = UINib(nibName: T.nibName, bundle: bundle)
         
         register(nib, forCellReuseIdentifier: T.defaultReuseIdentifier)
     }
     
-    func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T where T: ReusableView {
+    func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T {
         guard let cell = dequeueReusableCell(withIdentifier: T.defaultReuseIdentifier, for: indexPath) as? T else {
             fatalError("Could not dequeue cell with identifier: \(T.defaultReuseIdentifier)")
         }
@@ -55,11 +55,11 @@ extension UITableView {
 
 extension UICollectionView {
     
-    func register<T: UICollectionViewCell>(_: T.Type) where T: ReusableView {
+    func register<T: UICollectionViewCell>(_: T.Type) {
         register(T.self, forCellWithReuseIdentifier: T.defaultReuseIdentifier)
     }
     
-    func register<T: UICollectionViewCell>(_: T.Type) where T: ReusableView, T: NibLoadableView {
+    func register<T: UICollectionViewCell>(_: T.Type) where T: NibLoadableView {
         let bundle = Bundle(for: T.self)
         let nib = UINib(nibName: T.nibName, bundle: bundle)
         
@@ -72,7 +72,7 @@ extension UICollectionView {
                  withReuseIdentifier: T.defaultReuseIdentifier)
     }
     
-    func dequeueReusableCell<T: UICollectionViewCell>(forIndexPath indexPath: IndexPath) -> T where T: ReusableView {
+    func dequeueReusableCell<T: UICollectionViewCell>(forIndexPath indexPath: IndexPath) -> T {
         guard let cell = dequeueReusableCell(withReuseIdentifier: T.defaultReuseIdentifier, for: indexPath) as? T else {
             fatalError("Could not dequeue cell with identifier: \(T.defaultReuseIdentifier)")
         }
