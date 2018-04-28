1def52e5f8f10d4fdf0f47a4dc743ee0be913cae
Julio Andres <julioacarrettoni@gmail.com>
2018-04-28 12:20:43 -0300

    demo-tvOS Added the tvOS Target and a Single View Project

---
+++ Example/EasyTransitionTVOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyTransitionTVOS
+//
+//  Created by Julio Carrettoni on 28/04/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
