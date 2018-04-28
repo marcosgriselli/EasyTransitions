80004ee881a90de05df4ec528b40ada56de9a749
Julio Andres <julioacarrettoni@gmail.com>
2018-04-28 14:53:35 -0300

    demo-tvOS some project refactoring

---
+++ Example/EasyTransitionTVOS/Classes/ViewControllers/MovieDetai/MovieDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MovieDetailViewController.swift
+//  EasyTransitionTVOS
+//
+//  Created by Julio Carrettoni on 28/04/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
