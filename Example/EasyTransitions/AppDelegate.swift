b3e58f1b77116fbd77c62cb27ca03ea5455d990c
Marcos Griselli <marcosgriselli@gmail.com>
2018-07-07 11:47:07 -0300

    default today transition

--- Example/EasyTransitions/AppDelegate.swift
+++ Example/EasyTransitions/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         
         // Modal
-//        window?.rootViewController = TodayCollectionViewController()
+        window?.rootViewController = TodayCollectionViewController()
         
         // Navigation
 //        let navigation = UINavigationController(rootViewController: GalleryTableViewController())
@@ -27,7 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //        window?.rootViewController = navigation
         
         // Presentation
-        window?.rootViewController = BasePresentationViewController()
+//        window?.rootViewController = BasePresentationViewController()
         window?.makeKeyAndVisible()
         return true
     }
